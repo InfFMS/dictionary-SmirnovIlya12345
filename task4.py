b6fb769d068a7042aad169b019932be465aa97f7
Tutor <Tutor@main.nsu.ru>
2025-04-28 14:14:20 +0700

    half

--- task4.py
+++ task4.py
@@ -10,3 +10,44 @@ numbers = {'dict1': [11, 20, 30, 17, 6, 24, 90, 15, 17],
           	'dict9': [12, 70, 18, 28, 18, 28, 53, 91],
           	'dict10': [29, 79, 18, 28, 18, 28, 32, 55]}
 # Напишите программу, которая удалит из значений словаря все четные числа.
+list1=[]
+list2=[]
+list3=[]
+list4=[]
+list5=[]
+list6=[]
+list7=[]
+list8=[]
+list9=[]
+list10=[]
+for i in range(len(numbers['dict1'])):
+	if numbers['dict1'][i]%2==1:
+		list1.append(numbers['dict1'][i])
+for i in range(len(numbers['dict2'])):
+	if numbers['dict2'][i]%2==1:
+		list2.append(numbers['dict2'][i])
+for i in range(len(numbers['dict3'])):
+	if numbers['dict3'][i]%2==1:
+		list3.append(numbers['dict3'][i])
+for i in range(len(numbers['dict4'])):
+	if numbers['dict4'][i]%2==1:
+		list4.append(numbers['dict4'][i])
+for i in range(len(numbers['dict5'])):
+	if numbers['dict5'][i]%2==1:
+		list5.append(numbers['dict5'][i])
+for i in range(len(numbers['dict6'])):
+	if numbers['dict6'][i]%2==1:
+		list6.append(numbers['dict6'][i])
+for i in range(len(numbers['dict7'])):
+	if numbers['dict7'][i]%2==1:
+		list7.append(numbers['dict7'][i])
+for i in range(len(numbers['dict8'])):
+	if numbers['dict8'][i]%2==1:
+		list8.append(numbers['dict8'][i])
+for i in range(len(numbers['dict9'])):
+	if numbers['dict9'][i]%2==1:
+		list9.append(numbers['dict9'][i])
+for i in range(len(numbers['dict10'])):
+	if numbers['dict10'][i]%2==1:
+		list10.append(numbers['dict10'][i])
+print(list1,list2,list3,list4,list5,list6,list7,list8,list9,list10,sep='\n')
