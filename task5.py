6a2acae21dec459ac2ab21b7b1281416817868b0
SmirnovIlya12345 <betelgeuse08si@yandex.ru>
2025-05-05 14:11:47 +0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -6,4 +6,11 @@
 # Используйте словари для словаря:)
 my_dict = {'привет': 'hello', 'как дела': 'how are you', 'мысль': 'thought', 'сквозь': 'through', 'хотя': 'although', 'кашель': 'сough', 'сук': 'bough', 'твёрдый': 'tough'}
 print('Enter a russian word')
-print(my_dict[input()])
+b=str(input())
+if b in my_dict:
+    print(my_dict[b])
+else:
+    print('Enter a translation')
+    с=input()
+    my_dict[b]=с
+    print(my_dict.get(b))
